f2bd3e8d21b6140d460717e1c4d75a4667621806
Михаил Щербаков <ScherbakovMS@yandex.ru>
2023-02-18 15:50:35 +0400

    Функция для проверки строки на палиндром

--- js/functions.js
+++ js/functions.js
@@ -2,3 +2,9 @@ function checkStringMatchesLength(sourceString, needLenth){
   return sourceString.length === needLenth;
 }
 
+function checkStringIsPalindrome(sourceString){
+  sourceString = (sourceString ?? '').replace(/\s/g, '');
+  const srcArray = sourceString.split('');
+  const reverseString = srcArray.reverse().join('');
+  return reverseString.toLowerCase() === sourceString.toLowerCase();
+}

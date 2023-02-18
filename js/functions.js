82448698f29400c2f9c9c0f50f30fe7182237520
Михаил Щербаков <ScherbakovMS@yandex.ru>
2023-02-18 16:06:09 +0400

    Функция извлечения целого положительного числа из строки\дробного числа

--- js/functions.js
+++ js/functions.js
@@ -8,3 +8,13 @@ function checkStringIsPalindrome(sourceString){
   const reverseString = srcArray.reverse().join('');
   return reverseString.toLowerCase() === sourceString.toLowerCase();
 }
+
+function extractIntegerFromString(sourceValue){
+  const srcString = `${sourceValue ?? ''}`;
+  const integerCharsString = srcString.replace(/[^0-9]/g, '');
+  if (integerCharsString.length === 0) {
+    return NaN;
+  }
+  return parseFloat(integerCharsString);
+}
+

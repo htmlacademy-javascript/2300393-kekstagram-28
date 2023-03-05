429659802095e6014233921fa2583b8eeadc5fcd
Михаил Щербаков <ScherbakovMS@yandex.ru>
2023-03-05 16:14:33 +0400

    Сделал отрисовку элементов по заданию

--- js/draw-thumbnails.js
+++ js/draw-thumbnails.js
@@ -1,26 +1,38 @@
 import {getMockPhotosArray} from './mock-data.js';
 
+const setPhotoData = (templateClone, photo) => {
+  const img = templateClone.querySelector('.picture__img');
+  img.src = photo.url;
+
+  const likesCounter = templateClone.querySelector('.picture__likes');
+  likesCounter.textContent = photo.likes;
+
+  const commentsCounter = templateClone.querySelector('.picture__comments');
+  commentsCounter.textContent = photo.comments.length;
+};
+
+const getTemplateClone = () => {
+  const thumbnailsTemplate = document.querySelector('#picture').content;
+  return thumbnailsTemplate.cloneNode(true);
+};
+
+const getCloneWithPhotoData = (photo) => {
+  const templateClone = getTemplateClone();
+  setPhotoData(templateClone, photo);
+  return templateClone;
+};
+
 const drawThumbnails = () => {
   const photosArray = getMockPhotosArray();
-  const thumbnailsTemplate = document.querySelector('#picture').content;
-
-  const picturesBlock = document.querySelector('.pictures');
 
   const fragment = document.createDocumentFragment();
 
   for(const photo of photosArray){
-    const templateClone = thumbnailsTemplate.cloneNode(true);
-
-    const cloneImg = templateClone.querySelector('.picture__img');
-    cloneImg.src = photo.url;
-
-
-    //console.log(photo);
-    fragment.appendChild(templateClone);
+    fragment.appendChild(getCloneWithPhotoData(photo));
   }
-  //console.log(photosArray);
+
+  const picturesBlock = document.querySelector('.pictures');
   picturesBlock.appendChild(fragment);
 };
-
 
 export {drawThumbnails};

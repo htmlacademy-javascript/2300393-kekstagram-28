fc46118eadd19d145217b811c0de9fdc58994c49
Михаил Щербаков <ScherbakovMS@yandex.ru>
2023-03-18 22:32:54 +0400

    Набор правил валидации хештегов..

--- js/form-validation-helper.js
+++ js/form-validation-helper.js
@@ -3,6 +3,49 @@ import { isEscapeKey } from './full-size-evt-helper.js';
 const uploadFileControl = document.querySelector('#upload-file');
 const uploadOverlay = document.querySelector('.img-upload__overlay');
 const imgUploadCancel = document.querySelector('.img-upload__cancel');
+const form = document.querySelector('.img-upload__form');
+const hashtagInput = document.querySelector('.text__hashtags');
+
+const pristine = new Pristine(form);
+
+const validateHashtag = (thisTags) => {
+  if (!thisTags || /\s+/.test()) {
+    return true;
+  }
+  const tagRegex = /^#[а-яёa-z0-9]{1,20}$/i;
+  const existTags = [];
+  const tags = thisTags.split(/\s+/);
+  if (tags.length > 5) {
+    return false;
+  }
+  for (const tag of tags) {
+
+    if (!tagRegex.test(tag)) {
+      return false;
+    }
+
+    existTags.push(tag.toUpperCase());
+    if (existTags.filter((e) => e === tag.toUpperCase()).length > 1) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const getHashtagErrorMessage = () => 'Хештеги не удовлетворяют правилам!';
+
+
+pristine.addValidator(hashtagInput, validateHashtag, getHashtagErrorMessage);
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  if (!pristine.validate(hashtagInput)) {
+    console.log('неа');
+  }
+  else{
+    console.log('угу');
+  }
+});
 
 const closeValidationForm = () => {
   uploadOverlay.classList.add('hidden');
@@ -13,7 +56,7 @@ const closeValidationForm = () => {
   document.querySelectorAll('.effects__radio').forEach((e) => {
     e.value = 'none';
   });
-  document.querySelector('.text__hashtags').value = '';
+  hashtagInput.value = '';
 };
 
 const setValidationEventListeners = () => {

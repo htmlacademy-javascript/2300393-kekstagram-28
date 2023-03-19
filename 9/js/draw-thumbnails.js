2afb8ea74fd9736c0133651c46ea5990aeaeeb64
Keks <keks@htmlacademy.ru>
2023-03-19 17:54:59 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/draw-thumbnails.js
@@ -0,0 +1,38 @@
+import {getMockPhotosArray} from './mock-data.js';
+
+const setPhotoData = (templateClone, photo) => {
+  const img = templateClone.querySelector('.picture__img');
+  img.src = photo.url;
+  img.dataset.id = photo.id;
+  const likesCounter = templateClone.querySelector('.picture__likes');
+  likesCounter.textContent = photo.likes;
+
+  const commentsCounter = templateClone.querySelector('.picture__comments');
+  commentsCounter.textContent = photo.comments.length;
+};
+
+const getTemplateClone = () => {
+  const thumbnailsTemplate = document.querySelector('#picture').content;
+  return thumbnailsTemplate.cloneNode(true);
+};
+
+const getCloneWithPhotoData = (photo) => {
+  const templateClone = getTemplateClone();
+  setPhotoData(templateClone, photo);
+  return templateClone;
+};
+
+const drawThumbnails = (dataArray) => {
+  const photosArray = dataArray ?? getMockPhotosArray();
+
+  const fragment = document.createDocumentFragment();
+
+  for(const photo of photosArray){
+    fragment.appendChild(getCloneWithPhotoData(photo));
+  }
+
+  const picturesBlock = document.querySelector('.pictures');
+  picturesBlock.appendChild(fragment);
+};
+
+export {drawThumbnails};

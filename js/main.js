5e9f40c0c57599a3c83f9e8c043e3500077f7e32
Михаил Щербаков <ScherbakovMS@yandex.ru>
2023-03-21 22:28:05 +0400

    Масштабирование изображения

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ import {drawThumbnails} from './draw-thumbnails.js';
 import {setFullSizeEventListeners} from './full-size-evt-helper.js';
 import {getMockPhotosArray} from './mock-data.js';
 import {setValidationEventListeners} from './form-validation-helper.js';
+import {setImgScaleEventListeners} from './img-scale-evt-helper.js';
 
 const photos = getMockPhotosArray();
 drawThumbnails(photos);
 setFullSizeEventListeners(photos);
 setValidationEventListeners();
+setImgScaleEventListeners();

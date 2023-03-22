e41d64b289fe745b8b5a85528decd85ba6bdab16
Keks <keks@htmlacademy.ru>
2023-03-22 21:17:05 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,13 @@
+import {drawThumbnails} from './draw-thumbnails.js';
+import {setFullSizeEventListeners} from './full-size-evt-helper.js';
+import {getMockPhotosArray} from './mock-data.js';
+import {setValidationEventListeners} from './form-validation-helper.js';
+import {setImgScaleEventListeners} from './img-scale-evt-helper.js';
+import { setEffectFieldsetEvt } from './nouislider-evt-helper.js';
+
+const photos = getMockPhotosArray();
+drawThumbnails(photos);
+setFullSizeEventListeners(photos);
+setValidationEventListeners();
+setImgScaleEventListeners();
+setEffectFieldsetEvt();

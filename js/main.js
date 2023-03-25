e1c263474f28f625122442ca259bbec2a2121c06
Михаил Щербаков <ScherbakovMS@yandex.ru>
2023-03-25 17:03:31 +0400

    Подключил получение данных

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
-import {drawThumbnails} from './draw-thumbnails.js';
-import {setFullSizeEventListeners} from './full-size-evt-helper.js';
-import {getMockPhotosArray} from './mock-data.js';
-import {setValidationEventListeners} from './form-validation-helper.js';
-import {setImgScaleEventListeners} from './img-scale-evt-helper.js';
+import { drawThumbnails } from './draw-thumbnails.js';
+import { setFullSizeEventListeners } from './full-size-evt-helper.js';
+//import { getMockPhotosArray } from './mock-data.js';
+import { setValidationEventListeners } from './form-validation-helper.js';
+import { setImgScaleEventListeners } from './img-scale-evt-helper.js';
 import { setEffectFieldsetEvt } from './nouislider-evt-helper.js';
+import { getData, sendData } from './fetch-helper.js';
 
-const photos = getMockPhotosArray();
-drawThumbnails(photos);
-setFullSizeEventListeners(photos);
+//const photos = getMockPhotosArray();
+
+getData().then((photos) => {
+  drawThumbnails(photos);
+  setFullSizeEventListeners(photos);
+});
+
+//drawThumbnails(photos);
+//setFullSizeEventListeners(photos);
 setValidationEventListeners();
 setImgScaleEventListeners();
 setEffectFieldsetEvt();
